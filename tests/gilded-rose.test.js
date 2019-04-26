ca0258bdd8c95bed690615f9165460d1e2107fd9
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-04-26 17:47:00 +0800

    Add tests

--- tests/gilded-rose.test.js
+++ tests/gilded-rose.test.js
@@ -8,45 +8,99 @@ describe("Gilded Rose", () => {
     expect(items[0]).toEqual(new Item("Normal Item", -1, 3));
   });
 
-  it.todo("should not decrease an item's quality below zero");
+  it("should not decrease an item's quality below zero", () => {
+    const shop = new Shop([new Item("Normal Item", 0, 1)]);
+    const items = shop.updateQuality();
 
-  it.todo("should increase the quality of 'Aged Brie' the older it gets");
+    expect(items[0]).toEqual(new Item("Normal Item", -1, 0));
+  });
 
-  it.todo("should not increase an item's quality above 50");
+  it("should increase the quality of 'Aged Brie' the older it gets", () => {
+    const shop = new Shop([new Item("Aged Brie", 10, 10)]);
+    const items = shop.updateQuality();
 
-  it.todo(
-    "should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality < 50 "
-  );
+    expect(items[0].quality).toBe(11);
+  });
 
-  it.todo(
-    "should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality > 50"
-  );
+  it("should not increase an item's quality above 50", () => {
+    const shop = new Shop([new Item("Aged Brie", 9, 50)]);
+    const items = shop.updateQuality();
+    expect(items[0].quality).toBe(50);
+  });
 
-  it.todo(
-    "should increase the quality of 'Backstage passes' by 2, 10 - 6 days before the concert"
-  );
+  it("should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality < 50 ", () => {
+    const shop = new Shop([new Item("Sulfuras, Hand of Ragnaros", 9, 49)]);
+    const items = shop.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(49);
+  });
 
-  it.todo(
-    "should not increase the quality of 'Backstage passes' by 2, 9  days before the concert"
-  );
+  it("should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality > 50", () => {
+    const shop = new Shop([new Item("Sulfuras, Hand of Ragnaros", 9, 51)]);
+    const items = shop.updateQuality();
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(51);
+  });
 
-  it.todo(
-    "should not increase the quality of 'Backstage passes' by 2, 5 days before the concert"
-  );
+  it("should increase the quality of 'Backstage passes' by 2, 10 - 6 days before the concert", () => {
+    const shop = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
+    ]);
+    const quality = 0;
+    let items;
+    // eslint-disable-next-line
+    for (let i = 10; i <= 6; i--) {
+      items = shop.updateQuality();
+      expect(items[0]).toEqual(
+        new Item(
+          "Backstage passes to a TAFKAL80ETC concert",
+          i - 1,
+          quality + 2
+        )
+      );
+    }
+  });
 
-  it.todo(
-    "should increase the quality of 'Backstage passes' by 3, 5 - 1 day before the concert"
-  );
+  it("should not increase the quality of 'Backstage passes' by 2, 11  days before the concert", () => {
+    const shop = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 5)
+    ]);
+    const items = shop.updateQuality();
 
-  it.todo(
-    "should not increase the quality of 'Backstage passes' by 3, 4 day before the concert"
-  );
+    expect(items[0].quality).toBe(6);
+  });
 
-  it.todo(
-    "should decrease the quality of 'Backstage passes' to 0, after the concert"
-  );
+  it("should increase the quality of 'Backstage passes' by 3, 5 days before the concert", () => {
+    const shop = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5)
+    ]);
+    const items = shop.updateQuality();
 
-  it.todo(
-    "should decrease the quality of 'conjured' twice as fast as normal items before expiry"
-  );
+    expect(items[0].quality).toBe(8);
+  });
+
+  it("should not increase the quality of 'Backstage passes' by 3, 4 day before the concert", () => {
+    const shop = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 5)
+    ]);
+    const items = shop.updateQuality();
+
+    expect(items[0].quality).toBe(8);
+  });
+
+  it("should decrease the quality of 'Backstage passes' to 0, after the concert", () => {
+    const shop = new Shop([
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)
+    ]);
+    const items = shop.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+
+  it.skip("should decrease the quality of 'conjured' twice as fast as normal items before expiry", () => {
+    const shop = new Shop([new Item("Conjured stuff", 4, 5)]);
+    const items = shop.updateQuality();
+
+    expect(items[0].quality).toBe(3);
+  });
 });
