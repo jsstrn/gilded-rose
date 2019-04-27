dcd84743ae35075e150d3c528578649d8a588e1e
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-04-27 23:43:10 +0800

    Update tests

--- tests/gilded-rose.test.js
+++ tests/gilded-rose.test.js
@@ -1,106 +1,110 @@
 const { Shop, Item } = require("../src/gilded-rose");
 
+function itemAfterUpdate(itemName, daysTillSellByDate, quality) {
+  const items = [new Item(itemName, daysTillSellByDate, quality)];
+  const shop = new Shop(items);
+  const item = shop.updateQuality()[0];
+  return item;
+}
+
 describe("Gilded Rose", () => {
-  it("should degrade the quality twice as fast after sell by date", () => {
-    const shop = new Shop([new Item("Normal Item", 0, 5)]);
-    const items = shop.updateQuality();
+  const itemName = "Regular Item";
 
-    expect(items[0]).toEqual(new Item("Normal Item", -1, 3));
+  describe("For regular items", () => {
+    it("degrades quality by 2 if it is past its sell by date", () => {
+      const daysTillSellByDate = 0;
+      const quality = 5;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(3);
+    });
+
+    it("should not degrade quality to less than 0", () => {
+      const daysTillSellByDate = 0;
+      const quality = 0;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(0);
+    });
   });
 
-  it("should not decrease an item's quality below zero", () => {
-    const shop = new Shop([new Item("Normal Item", 0, 1)]);
-    const items = shop.updateQuality();
+  describe("For items labeled Aged Brie", () => {
+    const itemName = "Aged Brie";
 
-    expect(items[0]).toEqual(new Item("Normal Item", -1, 0));
+    it("increases quality the older it gets", () => {
+      const daysTillSellByDate = 5;
+      const quality = 0;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(1);
+    });
+
+    it("should not increase quality above 50", () => {
+      const daysTillSellByDate = 5;
+      const quality = 50;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(50);
+    });
   });
 
-  it("should increase the quality of 'Aged Brie' the older it gets", () => {
-    const shop = new Shop([new Item("Aged Brie", 10, 10)]);
-    const items = shop.updateQuality();
+  describe("For items labeled Sulfaras", () => {
+    const itemName = "Sulfuras, Hand of Ragnaros";
 
-    expect(items[0].quality).toBe(11);
+    it("should not alter sell by date", () => {
+      const daysTillSellByDate = 5;
+      const quality = 40;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.sellIn).toBe(5);
+    });
+
+    it("should not alter quality", () => {
+      const daysTillSellByDate = 5;
+      const quality = 40;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(40);
+    });
   });
 
-  it("should not increase an item's quality above 50", () => {
-    const shop = new Shop([new Item("Aged Brie", 9, 50)]);
-    const items = shop.updateQuality();
-    expect(items[0].quality).toBe(50);
+  describe("For items labeled Backstage Passes", () => {
+    const itemName = "Backstage passes to a TAFKAL80ETC concert";
+
+    it("increases quality by 1 when sell by date is 15 days", () => {
+      const daysTillSellByDate = 15;
+      const quality = 20;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(21);
+    });
+
+    it("increases quality by 2 when sell by date is 10 days", () => {
+      const daysTillSellByDate = 10;
+      const quality = 20;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(22);
+    });
+
+    it("increases quality by 3 when sell by date is 5 days", () => {
+      const daysTillSellByDate = 5;
+      const quality = 20;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(23);
+    });
+
+    it("decreases the quality to 0 when sell by date is 0 days", () => {
+      const daysTillSellByDate = 0;
+      const quality = 20;
+      const item = itemAfterUpdate(itemName, daysTillSellByDate, quality);
+
+      expect(item.quality).toBe(0);
+    });
   });
 
-  it("should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality < 50 ", () => {
-    const shop = new Shop([new Item("Sulfuras, Hand of Ragnaros", 9, 49)]);
-    const items = shop.updateQuality();
-    expect(items[0].sellIn).toBe(9);
-    expect(items[0].quality).toBe(49);
-  });
-
-  it("should not decrease or increase the sell in or quality of Sulfuras a legendary item with quality > 50", () => {
-    const shop = new Shop([new Item("Sulfuras, Hand of Ragnaros", 9, 51)]);
-    const items = shop.updateQuality();
-    expect(items[0].sellIn).toBe(9);
-    expect(items[0].quality).toBe(51);
-  });
-
-  it("should increase the quality of 'Backstage passes' by 2, 10 - 6 days before the concert", () => {
-    const shop = new Shop([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0)
-    ]);
-    const quality = 0;
-    let items;
-    // eslint-disable-next-line
-    for (let i = 10; i <= 6; i--) {
-      items = shop.updateQuality();
-      expect(items[0]).toEqual(
-        new Item(
-          "Backstage passes to a TAFKAL80ETC concert",
-          i - 1,
-          quality + 2
-        )
-      );
-    }
-  });
-
-  it("should not increase the quality of 'Backstage passes' by 2, 11  days before the concert", () => {
-    const shop = new Shop([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 11, 5)
-    ]);
-    const items = shop.updateQuality();
-
-    expect(items[0].quality).toBe(6);
-  });
-
-  it("should increase the quality of 'Backstage passes' by 3, 5 days before the concert", () => {
-    const shop = new Shop([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 5)
-    ]);
-    const items = shop.updateQuality();
-
-    expect(items[0].quality).toBe(8);
-  });
-
-  it("should not increase the quality of 'Backstage passes' by 3, 4 day before the concert", () => {
-    const shop = new Shop([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 4, 5)
-    ]);
-    const items = shop.updateQuality();
-
-    expect(items[0].quality).toBe(8);
-  });
-
-  it("should decrease the quality of 'Backstage passes' to 0, after the concert", () => {
-    const shop = new Shop([
-      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 5)
-    ]);
-    const items = shop.updateQuality();
-
-    expect(items[0].quality).toBe(0);
-  });
-
-  it.skip("should decrease the quality of 'conjured' twice as fast as normal items before expiry", () => {
-    const shop = new Shop([new Item("Conjured stuff", 4, 5)]);
-    const items = shop.updateQuality();
-
-    expect(items[0].quality).toBe(3);
+  describe("For items labeled Conjured", () => {
+    it.todo("decreases quality by 2 before expiry");
   });
 });
