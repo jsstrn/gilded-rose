2ad48275103fe2b45f7f811a6e9fab2f32607d8f
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-04-28 16:51:36 +0800

    Use new update method for Sulfuras items

--- src/Item.js
+++ src/Item.js
@@ -21,7 +21,7 @@ class Item {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
-    this.type = "";
+    this.type = null;
     this.setType(name);
   }
 
@@ -43,11 +43,14 @@ class Item {
   }
 
   update() {
+    if (this.name === SULFURAS) {
+      this.type.update();
+      return;
+    }
+
     if (this.name != AGED_BRIE && this.name != BACKSTAGE_PASS) {
       if (this.quality > 0) {
-        if (this.name != SULFURAS) {
-          this.quality = this.quality - 1;
-        }
+        this.quality = this.quality - 1;
       }
     } else {
       if (this.quality < 50) {
@@ -66,16 +69,14 @@ class Item {
         }
       }
     }
-    if (this.name != SULFURAS) {
-      this.sellIn = this.sellIn - 1;
-    }
+
+    this.sellIn = this.sellIn - 1;
+
     if (this.sellIn < 0) {
       if (this.name != AGED_BRIE) {
         if (this.name != BACKSTAGE_PASS) {
           if (this.quality > 0) {
-            if (this.name != SULFURAS) {
-              this.quality = this.quality - 1;
-            }
+            this.quality = this.quality - 1;
           }
         } else {
           this.quality = this.quality - this.quality;
